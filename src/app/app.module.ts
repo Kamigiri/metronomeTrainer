36c0fe89bb9d3f80f27a12b13811c80941aa543c
Mert <kamigiridev@gmail.com>
2023-10-29 18:46:22 +0100

    init metronome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MetronomeModule } from 'src/metronome/metronome.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MetronomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/metronome/metronome.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metronome',
+  templateUrl: './metronome.component.html',
+  styleUrls: ['./metronome.component.css'],
+})
+export class MetronomeComponent implements OnInit {
+  ac: AudioContext;
+  source: AudioBufferSourceNode;
+  frequency: number = 330;
+  phase: number = 0;
+  amp: number = 1;
+  input: string = '133';
+  constructor() {
+    this.ac = new AudioContext();
+    this.source = this.ac.createBufferSource();
+  }
+
+  ngOnInit() {
+    const buffer: AudioBuffer = this.ac.createBuffer(
+      1,
+      this.ac.sampleRate * 2,
+      this.ac.sampleRate
+    );
+    const channel: Float32Array = buffer.getChannelData(0);
+    const duration_frames: number = this.ac.sampleRate / 50;
+
+    for (var i = 0; i < duration_frames; i++) {
+      channel[i] = Math.sin(this.phase) * this.amp;
+      this.phase += (2 * Math.PI * this.frequency) / this.ac.sampleRate;
+      if (this.phase > 2 * Math.PI) {
+        this.phase -= 2 * Math.PI;
+      }
+      this.amp -= 1 / duration_frames;
+    }
+
+    this.source.buffer = buffer;
+    this.source.loop = true;
+    this.source.loopEnd = 1 / (this.getTempo() / 60);
+
+    this.source.connect(this.ac.destination);
+    this.source.start(0);
+  }
+  changeInputValue(event: Event) {
+    this.input = (event.target as HTMLInputElement).value;
+    this.source.loopEnd = 1 / (this.getTempo() / 60);
+  }
+
+  buttonHandler() {
+    if (this.ac.state == 'running') {
+      this.ac.suspend();
+    } else {
+      this.ac.resume();
+    }
+  }
+
+  private getTempo() {
+    return this.clamp(parseFloat(this.input));
+  }
+
+  private clamp(input: number, min: number = 30, max: number = 300) {
+    return Math.min(max, Math.max(min, input));
+  }
+}
